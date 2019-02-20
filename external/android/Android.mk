885efb96a3d033dff38352fe5ab07a1f64dd5937
maoanjie <maoanjie@qq.com>
2019-02-20 14:48:25 +0800

    库上传

--- external/android/Android.mk
+++ external/android/Android.mk
@@ -11,16 +11,6 @@ include $(PREBUILT_STATIC_LIBRARY)
 #======================================
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := cocos_freetype2_static
-LOCAL_MODULE_FILENAME := freetype2
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libfreetype.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/freetype
-
-include $(PREBUILT_STATIC_LIBRARY)
-
-#======================================
-include $(CLEAR_VARS)
-
 LOCAL_MODULE := cocos_jpeg_static
 LOCAL_MODULE_FILENAME := jpeg
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libjpeg.a
@@ -39,20 +29,12 @@ include $(PREBUILT_STATIC_LIBRARY)
 #======================================
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := cocos_chipmunk_static
-LOCAL_MODULE_FILENAME := chipmunk
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libchipmunk.a
-
-include $(PREBUILT_STATIC_LIBRARY)
-
-#======================================
-include $(CLEAR_VARS)
-
+ifeq ($(USE_TIFF),1)
 LOCAL_MODULE := cocos_tiff_static
 LOCAL_MODULE_FILENAME := tiff
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libtiff.a
-
 include $(PREBUILT_STATIC_LIBRARY)
+endif
 
 #======================================
 include $(CLEAR_VARS)
@@ -98,39 +80,6 @@ include $(PREBUILT_STATIC_LIBRARY)
 
 #======================================
 include $(CLEAR_VARS)
-
-LOCAL_MODULE := cocos_mozglue_static
-LOCAL_MODULE_FILENAME := mozglue
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libmozglue.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-#======================================
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := spidermonkey_static
-LOCAL_MODULE_FILENAME := js_static
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libjs_static.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/spidermonkey
-
-LOCAL_CPPFLAGS := -D__STDC_LIMIT_MACROS=1 -Wno-invalid-offsetof
-LOCAL_EXPORT_CPPFLAGS := -D__STDC_LIMIT_MACROS=1 -Wno-invalid-offsetof
-LOCAL_STATIC_LIBRARIES += cocos_mozglue_static
-include $(PREBUILT_STATIC_LIBRARY)
-
-#======================================
-include $(CLEAR_VARS)
-LOCAL_MODULE := v8_builtins_generators
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libv8_builtins_generators.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-#======================================
-include $(CLEAR_VARS)
-LOCAL_MODULE := v8_builtins_setup
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libv8_builtins_setup.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-#======================================
-include $(CLEAR_VARS)
 LOCAL_MODULE := v8_libbase
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libv8_libbase.a
 include $(PREBUILT_STATIC_LIBRARY)
@@ -149,8 +98,8 @@ include $(PREBUILT_STATIC_LIBRARY)
 
 #======================================
 include $(CLEAR_VARS)
-LOCAL_MODULE := v8_nosnapshot
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libv8_nosnapshot.a
+LOCAL_MODULE := v8_snapshot
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libv8_snapshot.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 #======================================
@@ -166,7 +115,7 @@ LOCAL_MODULE := v8_static
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libv8_base.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/v8
 
-LOCAL_WHOLE_STATIC_LIBRARIES += v8_builtins_generators v8_builtins_setup v8_libbase v8_libplatform v8_libsampler v8_nosnapshot v8_inspector
+LOCAL_WHOLE_STATIC_LIBRARIES += v8_libbase v8_libplatform v8_libsampler v8_snapshot
 
 include $(PREBUILT_STATIC_LIBRARY)
 
@@ -179,16 +128,5 @@ LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libuv.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/uv
 include $(PREBUILT_STATIC_LIBRARY)
 
-#======================================
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := PluginProtocolStatic
-LOCAL_MODULE_FILENAME := libPluginProtocolStatic
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libPluginProtocolStatic.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/anysdk
-LOCAL_EXPORT_LDLIBS += -llog
-LOCAL_EXPORT_LDLIBS += -lz
-
-include $(PREBUILT_STATIC_LIBRARY)
 #======================================
 #$(call import-module,android/cpufeatures)
